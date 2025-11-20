dcbca07fbd20a9482a99177d7207e2f42368a2f9
tll471 <brodskyyury@gmail.com>
2025-11-20 11:47:11 +0200

    сделал все в 1 файле, так намного легче

--- domashka_here/main.cpp
+++ domashka_here/main.cpp
@@ -1,198 +1,109 @@
-//#include<iostream>
-//using namespace std;
-//
-//class Animal
-//{
-//protected:
-// 
-// 
-//	int weight;
-//	int height;
-//	int population;
-//	string type;
-//public:
-//	Animal(){}
-//	Animal(int w, int h, int p, string t)
-//	{
-//		weight = w;
-//		height = h;
-//		population = p;
-//		type = t;
-//	}
-//	void Print()
-//	{
-//		cout << "---Animal info---" << endl;
-//		cout << "Weight: " << weight << endl;
-//		cout << "Height: " << height << endl;
-//		cout << "Population: " << population << endl;
-//		cout << "Type: " << type << endl;
-//	}
-//	void Eat()
-//	{
-//		cout << type << " eat" << endl;
-//		cout << endl;
-//	}
-//	void Move()
-//	{
-//		cout << type << " move" << endl;
-//		cout << endl;
-//	}
-//};
-//class Elephant : public Animal
-//{
-//	int trunk_length;
-//	string mainland;
-//public:
-//	Elephant(){}
-//	Elephant(int w, int h, int p, string t, int trunk, string main) : Animal(w, h, p, t)
-//	{
-//		trunk_length = trunk;
-//		mainland = main;
-//	}
-//	void Print()
-//	{
-//		Animal::Print();
-//		cout << "---Elephant info---" << endl;
-//		cout << "Trunk length: " << trunk_length << endl;
-//		cout << "Mainland: " << mainland << endl;
-//		cout << endl;
-//	}
-//};
-//class Penguin : public Animal
-//{
-//	string mainland;
-//	string color;
-//public:
-//	Penguin(){}
-//	Penguin(int w, int h, int p, string t, string col, string main) : Animal(w, h, p, t)
-//	{
-//		mainland = main;
-//		color = col;
-//	}
-//	void Print()
-//	{
-//		Animal::Print();
-//		cout << "---Penguin info---" << endl;
-//		cout << "Color: " << color << endl;
-//		cout << "Mainland: " << mainland <<endl;
-//		cout << endl;
-//	}
-//};
-//class Parrot : public Animal
-//{
-//	bool can_speak;
-//	string color;
-//public:
-//	Parrot(){}
-//	Parrot(int w, int h, int p, string t, string col, bool can) : Animal(w, h, p, t)
-//	{
-//		can_speak = can;
-//		color = col;
-//	}
-//	void Print()
-//	{
-//		string n;
-//		if (can_speak)
-//		{
-//			n = "yes";
-//		}
-//		else
-//		{
-//			n = "no";
-//		}
-//		Animal::Print();
-//		cout << "---Parror info---" << endl;
-//		cout << "Can speak: " << n << endl;
-//		cout << "Color: " << color << endl;
-//		cout << endl;
-// 	}
-//};
-//class Shark : public Animal
-//{
-//	int bite_force;
-//public:
-//	Shark(){}
-//	Shark(int w, int h, int p, string t, int bite) : Animal(w, h, p, t)
-//	{
-//		bite_force = bite;
-//	}
-//	void Print()
-//	{
-//		Animal::Print();
-//		cout << "---Shark info---" << endl;
-//		cout << "Bite force: " << bite_force << endl;
-//		cout << endl;
-//	}
-//};
-//int main()
-//{
-//	Animal* animal = nullptr;
-//	int choise;
-//	cout << "What do you want to create?" << endl;
-//	cout << "1: Elephant" << endl;
-//	cout << "2: Penguin" << endl;
-//	cout << "3: Parrot" << endl;
-//	cout << "4: Shark" << endl;
-//	cin >> choise;
-//
-//	switch(choise)
-//	{
-//		case 1:
-//		{
-//			animal = new Elephant(300, 5, 100000, "Elephant", 2, "Asia");
-//			animal->Move();
-//			animal->Eat();
-//			animal->Print();
-//			break;
-//		}
-//		case 2:
-//		{
-//			animal = new Penguin(50, 1, 2000, "Penguin", "Grey", "Antarctica");
-//			animal->Move();
-//			animal->Eat();
-//			animal->Print();
-//			break;
-//		}
-//		case 3:
-//		{
-//			animal = new Parrot(1, 1, 500000, "Parrot", "Green", true);
-//			animal->Move();
-//			animal->Eat();
-//			animal->Print();
-//			break;
-//		}
-//		case 4:
-//		{
-//			animal = new Shark(120, 3, 149999, "Shark", 1000);
-//			animal->Move();
-//			animal->Eat();
-//			animal->Print();
-//			break;
-//		}
-//		default:
-//			cout << "Error!" << endl;
-//			break;
-//	}
-//
-//	delete animal;
-//
-//	/*Elephant obj1(300, 5, 100000, "Elephant", 2, "Asia");
-//	obj1.Print();
-//	obj1.Eat();
-//	obj1.Move();
-//
-//	Penguin obj2(50, 1, 2000, "Penguin", "Grey", "Antarctica");
-//	obj2.Print();
-//	obj2.Eat();
-//	obj2.Move();
-//
-//	Parrot obj3(1, 1, 500000, "Parrot", "Green", true);
-//	obj3.Print();
-//	obj3.Eat();
-//	obj3.Move();
-//
-//	Shark obj4(120, 3, 149999, "Shark", 1000);
-//	obj4.Print();
-//	obj4.Eat();
-//	obj4.Move();*/
-//}
+#include <iostream>
+using namespace std;
+
+class Figure
+{
+protected:
+
+    double m_value1;
+    double m_value2;
+
+public:
+
+    void SetDimension(const double value1)
+    {
+        SetDimension(value1, value1);
+    }
+    void SetDimension(const double value1, const double value2)
+    {
+        m_value1 = value1;
+        m_value2 = value2;
+    }
+    void ShowArea() const
+    {
+        cout << "Value is undefined" << endl;
+    }
+};
+
+class Circle : public Figure
+{
+public:
+    void ShowArea() const
+    {
+        cout << "Area of circle: " << 3.14159 * m_value1 * m_value2 << endl;
+    }
+};
+
+class Rectangle : public Figure
+{
+public:
+    void ShowArea() const
+    {
+        cout << "Area of rectangle: " << m_value1 * m_value2 << endl;
+    }
+};
+
+class Triangle : public Figure
+{
+public:
+    void ShowArea() const
+    {
+        cout << "Area of triangle: " << 0.5 * m_value1 * m_value2 << endl;
+    }
+};
+
+class Square : public Figure
+{
+public:
+    void ShowArea() const
+    {
+        cout << "Area of square: " << m_value1 * m_value2 << endl;
+    }
+};
+
+class Rhombus : public Figure
+{
+public:
+    void ShowArea() const
+    {
+        cout << "Area of rhombus: " << m_value1 * m_value2 / 2 << endl;
+    }
+};
+
+class Parallelogram : public Figure
+{
+public:
+    void ShowArea() const
+    {
+        cout << "Area of parallelogram: " << m_value1 * m_value2 << endl;
+    }
+};
+
+int main()
+{
+    Circle circle;
+    Rectangle rectangle;
+    Triangle triangle;
+    Square square;
+    Rhombus rhombus;
+    Parallelogram parallelogram;
+
+    circle.SetDimension(10.0);
+    circle.ShowArea();
+
+    rectangle.SetDimension(5.5, 10.0);
+    rectangle.ShowArea();
+
+    triangle.SetDimension(5.5, 10.0);
+    triangle.ShowArea();
+
+    square.SetDimension(5);
+    square.ShowArea();
+
+    rhombus.SetDimension(10, 20);
+    rhombus.ShowArea();
+
+    parallelogram.SetDimension(5, 10);
+    parallelogram.ShowArea();
+
+}
