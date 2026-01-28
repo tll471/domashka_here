47f65d75c0767bd0cca50ebcbe44712de91b60dd
tll471 <brodskyyury@gmail.com>
2026-01-28 11:50:22 +0200

    28.01

--- domashka_here/main.cpp
+++ domashka_here/main.cpp
@@ -1,71 +1,207 @@
 #include <iostream>
+#include <atomic>
 #include <thread>
-#include <mutex>
 #include <fstream>
-#include <Windows.h>
+#include <mutex>
 using namespace std;
 
-mutex m;
+//int arr[50];
+//atomic<int> counter = { 0 };
+//
+//void Fill()
+//{
+//	srand(time(0));
+//	for (size_t i = 0; i < 50; i++)
+//	{
+//		arr[i] = rand() % 41 - 20;
+//	}
+//}
+//
+//void Parni()
+//{
+//	for (size_t i = 0; i < 50; i++)
+//	{
+//		if (arr[i] % 2 == 0)
+//		{
+//			counter++;
+//			cout << "Парные элементы: " << arr[i] << endl;
+//		}
+//	}
+//}
+//
+//void Minus()
+//{
+//	for (size_t i = 0; i < 50; i++)
+//	{
+//		if (arr[i] < 0)
+//		{
+//			counter++;
+//			cout << "Отрицательные элементы: " << arr[i] << endl;
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	setlocale(LC_ALL, "RUSSIA");
+//	thread t1(Fill);
+//	thread t2(Parni);
+//	thread t3(Minus);
+//
+//	t1.join();
+//	t2.join();
+//	t3.join();
+//
+//	cout << endl;
+//	cout << "End..." << endl;
+//	cout << counter << endl;
+//}
 
-void Create()
-{
-	m.lock();
-	ofstream file("file.txt", ios::out);
-	if (file.is_open())
-	{
-		cout << "File is open!" << endl;
-	}
-	file.close();
-	m.unlock();
-}
 
-void Fill(int arr[], int size)
-{
-	m.lock();
-	ofstream file("file.txt", ios::out);
-	if (file.is_open())
-	{
-		for (size_t i = 0; i < size; i++)
-		{
-			arr[i] = rand() % 100 + 1;
-			file << arr[i] << " ";
-		}
-	}
-	file.close();
-	m.unlock();
-}
 
-void Print(int arr[], int size)
-{
-	m.lock();
-	ifstream file2("file.txt", ios::in);
-	string value;
+// 1
+//
+//mutex m;
+//
+//void Write1()
+//{
+//	m.lock();
+//	ofstream file("Text.txt", ios::out);
+//	if (file.is_open())
+//	{
+//		for (size_t i = 0; i < 10; i++)
+//		{
+//			file << i << endl;
+//		}
+//	}
+//	file.close();
+//	m.unlock();
+//}
+//void Write2()
+//{
+//	m.lock();
+//	ofstream file("Text.txt", ios::out);
+//	if (file.is_open())
+//	{
+//		for (size_t i = 0; i < 5; i++)
+//		{
+//			string name;
+//			cin >> name;
+//			file << name;
+//		}
+//	}
+//	file.close();
+//	m.unlock();
+//}
+//void Out()
+//{
+//	m.lock();
+//	ifstream file2("Text.txt", ios::in);
+//	string value;
+//	while (file2 >> value)
+//	{
+//		cout << value << endl;
+//	}
+//	m.unlock();
+//
+//}
+//
+//int main()
+//{
+//	thread t1(Write1);
+//	thread t2(Write2);
+//	thread t3(Out);
+//
+//	t1.join();
+//	t2.join();
+//	t3.join();
+//
+//
+//	cout << "END..." << endl;
+//}
 
-	while (file2 >> value)
-	{
-		cout << value << ", ";
-		Sleep(20);
-	}
-	file2.close();
-	m.unlock();
 
-}
 
-int main()
-{
-	srand(time(0));
-
-	const int size = 100;
-	int arr[size];
-
-	thread t1(Create);
-	thread t2(Fill, arr, size);
-	thread t3(Print, arr, size);
-
-	t1.join();
-	t2.join();
-	t3.join();
-
-	cout << endl;
-	cout << "Ends..." << endl;
-}
+// 2
+//
+//mutex m;
+//
+//void Fill()
+//{
+//	m.lock();
+//	ofstream file("input.txt", ios::out);
+//	if (file.is_open())
+//	{
+//		file << "Hello" << endl;
+//		file << "World" << endl;
+//		file << "Grrr" << endl;
+//		file << "Mobile" << endl;
+//		file << "Bye" << endl;
+//	}
+//	file.close();
+//	m.unlock();
+//}
+//void Create()
+//{
+//	m.lock();
+//	ofstream file("forbidden.txt", ios::out);
+//	if (file.is_open())
+//	{
+//		file << "Forbidden words:" << endl;
+//	}
+//	file.close();
+//	m.unlock();
+//}
+//void Find()
+//{
+//	m.lock();
+//
+//	ifstream file2("input.txt", ios::in);
+//	string value;
+//	while (file2 >> value)
+//	{
+//		if (value == "Grrr")
+//		{
+//			ofstream file("forbidden.txt", ios::out);
+//			if (file.is_open())
+//			{
+//				file << value << endl;
+//			}
+//			file.close();
+//		}
+//		cout << value << endl;
+//	}
+//	file2.close();
+//	m.unlock();
+//}
+//
+//void Answer()
+//{
+//	m.lock();
+//	ifstream file2("forbidden.txt", ios::in);
+//	string value;
+//	cout << "-----" << endl;
+//	while (file2 >> value)
+//	{
+//		cout << value << endl;
+//	}
+//	cout << "-----" << endl;
+//	file2.close();
+//	m.unlock();
+//}
+//
+//int main()
+//{
+//	thread t1(Fill);
+//	thread t2(Create);
+//	thread t3(Find);
+//	thread t4(Answer);
+//
+//	t1.join();
+//	t2.join();
+//	t3.join();
+//	t4.join();
+//
+//
+//	cout << "END..." << endl;
+//}
